80d536e7ccbea1ce058b0ba958c009fdd175b054
TRomesh <tharakaromesh@gmail.com>
2017-07-15 21:15:57 +0530

    configure firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyBeEDG-59hkxLkCsKSBieplyWHSeIBacks",
+  authDomain: "taximate-d1771.firebaseapp.com",
+  databaseURL: "https://taximate-d1771.firebaseio.com",
+  projectId: "taximate-d1771",
+  storageBucket: "taximate-d1771.appspot.com",
+  messagingSenderId: "600462699580"
+};
+firebase.initializeApp(config);
+export default firebase;
